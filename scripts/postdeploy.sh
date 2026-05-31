4f53fd25f021048083fc112c2a99e9f8712b3135
aleeasgar0404 <alee.asgar@gmail.com>
2026-05-31 20:50:29 +0530

    update

--- scripts/postdeploy.sh
+++ scripts/postdeploy.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+
+cd /home/ec2-user/deploy || exit 1
+
+export JAVA_HOME=/usr/lib/jvm/java-17-amazon-corretto.x86_64
+export PATH=$JAVA_HOME/bin:$PATH
+
 echo "Post deployment started"
 
-cd /home/ec2-user/deploy
 
-echo "Compiling Java file"
-javac cs_java/src/cs_java/Wish.java
 
 echo "Running Java program"
 java -cp cs_java/src cs_java.Wish
